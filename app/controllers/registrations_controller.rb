66f7499086248ad30039228dd9d16019ed89fac7
Adeja <alendor6@gmail.com>
2020-03-12 12:56:02 -0400

    added registration controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < ApplicationController
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+ 
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
+
+end
