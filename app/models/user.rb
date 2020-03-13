31d76a7c144a78589bbfbdfc00719842e6bddce4
Adeja <alendor6@gmail.com>
2020-03-13 16:15:53 -0400

    added has_many & self.from_omniauth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,28 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
+  has_many :watchlists
+  has_many :videos, through: :watchlists
 
-        def self.from_omniauth(auth)
-          # Either create a User record or update it based on the provider (Google) and the UID   
-          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-            user.token = auth.credentials.token
-            user.expires = auth.credentials.expires
-            user.expires_at = auth.credentials.expires_at
-            user.refresh_token = auth.credentials.refresh_token
+  validates :name, presence: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+        def self.from_omniauth(access_token)
+
+          data = access_token.info
+          user = User.find_by(email: data['email'])
+          
+          # Uncomment the section below if you want users to be created if they don't exist
+          unless user
+              user = User.create(
+                name: data["name"],
+                email: data['email'],
+                 password: Devise.friendly_token[0,20]
+              )
           end
+          user
         end
 
-  
 end
