6829baf9cf4f59d7effa15b280f5edfc8ec2a058
Adeja <alendor6@gmail.com>
2020-03-20 01:30:53 -0400

    added hashed out optional methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  # protected
+
+  # def after_sign_in_path_for(resource)
+  #   # return the path based on resource
+  # end
+
+  # def after_sign_out_path_for(scope)
+  #   # return the path based on scope
+  # end
+
+
 end
