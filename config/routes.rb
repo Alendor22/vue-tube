b652e88cab456bc791cae7562b644a79f391718e
Adeja <alendor6@gmail.com>
2020-03-22 18:39:25 -0400

    added comment to file

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   
+  # nested emails route
   resources :users, only: [:show] do 
     resources :emails
   end
