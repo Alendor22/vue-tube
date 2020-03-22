2035e4d3329f1808a6e7a4b4003be6185a450d03
Adeja <alendor6@gmail.com>
2020-03-22 18:36:30 -0400

    added comments to file

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,16 +2,20 @@ class EmailsController < ApplicationController
   before_action :set_user #, only: [:new, :index, :create, :update :show]
   before_action :set_email, only: [:show]
   
+  # implied render index action
   def index
   end
 
+  # new form action
   def new
     @email = current_user.sent_emails.build
   end
 
+  # implicit render show action
   def show
   end
 
+  # explicit render create email action
   def create
     @email = current_user.sent_emails.build(email_params)
 
@@ -23,16 +27,19 @@ class EmailsController < ApplicationController
       end
   end
 
+  # edit form action
   def edit
     set_email
   end
 
+  # explicit render update action
   def update
     set_email
     @email.update(email_params)
     redirect_to user_emails_path(current_user.id)
   end
 
+  # explicit render destroy action
   def destroy
     set_email.destroy
     flash[:notice] = "Email Deleted!"
@@ -49,10 +56,12 @@ class EmailsController < ApplicationController
     params.require(:email).permit(:receiver_id, :subject, :content, :read)
   end
 
+  # helper
   def set_email
     @email = Email.find_by_id(params[:id])
   end
 
+  # helper
   def set_user
     @user = User.find_by_id(params[:user_id])
   end
