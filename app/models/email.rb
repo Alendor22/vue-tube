8fb84e99f1b700649b030dcbb94d7802c4b6460f
Adeja <alendor6@gmail.com>
2020-03-21 21:13:02 -0400

    validates subject & content, fixed scope method

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,9 +3,9 @@ class Email < ApplicationRecord
   belongs_to :sender, class_name: "User", foreign_key: :sender_id
   belongs_to :receiver, class_name: "User", foreign_key: :receiver_id
 
-  validates :receiver, presence: true
+  validates :subject, :content, presence: true
 
   scope :unread, ->{where(read: false)}
-  scope :recent, ->{where('updated_at < ?', 5.minutes.ago)}
+  scope :recent, ->{where('updated_at > ?', Date.today)}
 
 end
